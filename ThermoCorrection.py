bd88e249ad58436054d78c94a691ff4658ff3415
benehiebl <112540446+benehiebl@users.noreply.github.com>
2022-11-05 11:28:30 +0100

    Add files via upload

---
+++ ThermoCorrection.py
@@ -0,0 +1,469 @@
+# -*- coding: utf-8 -*-
+"""
+======================
+ThermoCorrection.py
+======================
+version: 1.3
+Author: Bene Hiebl
+Date: 05.11.2021
+
+An atmospheric and emissivity correction for georeferenced thermal infrared images from close range sensing
+based on Caselles et al. (1996), Kodimalar et al. (2020), Wiecek (2011) and Minkina et al. (2016).
+
+Input requirements:
+•	Georeferenced thermo raster images (.tif) + timeinfo
+•	DSM
+•	FVC
+•	Red Band (optional)
+•	Camera location
+•	Vertical gradients air temperature and humidity
+•	Emissivity for soil/vegetation
+•	Radiance air, downwelling radiance (optional)
+
+define Input in Correction_Parameters.txt
+
+"""
+
+
+import os
+import math as m 
+import rasterio
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+
+
+
+
+##############################################################################################
+####   FUNCTIONS   
+##############################################################################################
+
+# reads information of paramterfile and returns dictionary
+def ReadParameterfile(wd, file_name):
+    "Reads Parameterfile from txt and give information to dictionary, returns dictionary"
+    
+    pfile = open(wd + "/" + file_name,"r")
+
+    # create dictionary from parameters
+    try:
+        pdict = {}
+        for line in pfile:
+    
+            # delete # comments 
+            if line.startswith("#") == False:
+                # delete certain spaces that are not part of the file
+                line = line.strip(" ")
+                # delete \n
+                line = line.strip("\n")
+                # tabulator as seperator 
+                sep_line = line.split("\t")
+                sep_line = list(sep_line)
+                # values to dictionary
+                pdict[sep_line[0]] = sep_line[2]
+        
+        pfile.close()
+    except:
+        print("Something's wrong with the Parameter file! Probably you missed a /t (Tab)...")
+        
+    # convert numbers to float    
+    for i in pdict:
+        try:
+            pdict[i] = float(pdict[i])
+        except:
+            pdict[i] = str(pdict[i])
+    
+        
+    return pdict
+
+### create output directory 
+def OutDirectory (out_dir):
+    "creates output directory"
+        
+    try:
+        os.mkdir(out_dir)
+    except OSError:
+        print (f"Creation of the directory {out_dir} failed")
+    else:
+        print (f"Successfully created the directory {out_dir} ")
+    return out_dir
+
+### creates filelist
+def FileList(in_dir):
+    "creates string list of files in specific folder (format: foldername, no /)"
+    
+    image_dir = os.scandir(in_dir)
+    imagenames =[]
+    for file in image_dir:
+        name = file.name
+        imagenames.append(name)
+    return imagenames
+
+### create fractional vegetation cover
+def FvFromVI (VI_array,soil_limit,vegetation_limit):
+    "Calculates fractional Vegetation cover from Vegetation index, returns Fv array"
+    fv_cover = VI_array.copy()
+    fv_cover[fv_cover < soil_limit] = 0
+    fv_cover = np.where(VI_array > soil_limit, ((VI_array-soil_limit)/(vegetation_limit-soil_limit))**2,fv_cover)
+    fv_cover = np.where(VI_array > vegetation_limit, 1, fv_cover)
+    
+    return fv_cover
+
+# calculates land surface emissivity from Fractional Cover with thresholds for soil and vegetation
+def LseFromFv (fv_cover,veg_lse,soil_lse,cavity):
+    "Calculates land surface emissivity from fractional vegetation cover (Caselles 1996),returns lse as array"
+    
+    # de_value = fv_cover.copy()
+    # de_value[de_value < 1] = cavity
+    # de_value = np.where(fv_cover == 0, 0, de_value)
+    lse = np.where(fv_cover != -99999, veg_lse*fv_cover + soil_lse*(1-fv_cover) + (4 * cavity * fv_cover*(1-fv_cover)),fv_cover)
+    
+    return lse
+
+# calculate max humidity for given temperature
+def MaxHumidity (T):
+    "Calculates max possible humidity at temperature T from empirical formula, returns humidity"
+    
+    H = ((611.2*m.exp((22.46*T)/(272.62+T)))/(461.51*T))
+    return H
+
+### get raster coordinates from pixel in array of certain geotransformation
+def RastCoordinates(i,j,transform,dsm_array):
+    "computes raster coordinates from array coordinates, returns list=(x,y,z)"
+    
+    x = j*transform[0] + transform[2]
+    y = i*transform[4] + transform[5]
+    z = dsm_array[i,j]
+    loc = (x, y, z)
+    return loc
+
+# profile_dsm["transform"]
+# RastCoordinates(490,390,profile_dsm["transform"],dsm)
+
+def Boltz():
+    o = (5454781984210512994952000000 * (m.pi)**5)/29438455734650141042413712126365436049
+    return o
+
+def SatVapourPressure(T_atm):
+    "Calculates saturation vapour pressure in Pa from atmospheric temperature in K"
+        
+    if T_atm >= 273.15:
+        ps = 288.68 * (1.098 + ((T_atm - 273.15)/100))**8.02
+    else:
+        ps = 4.689 * ((1.486 + ((T_atm - 273.15)/100))**12.3)
+        
+    return ps
+
+def SatVapourPressureKG(T_atm):
+    "Calculates saturation vapour pressure g/kg-3 from atmospheric temperature in K"
+    
+    if T_atm >= 273.15:
+        ps = 6.112 * m.exp(17.269*((T_atm-273.17)/(T_atm-35.86)))
+    else:
+        ps = 6.112 * m.exp(21.874*((T_atm-273.17)/(T_atm-7.66)))
+        
+    return ps
+
+###################################################################################################
+####   Start MAIN SCRIPT
+###############################################################################################
+
+### Read Parameter file and give to dictionary
+print("Reading Parameter file.")
+### Set working directory 
+wd = os.path.dirname(os.path.realpath("ThermoCorrection.py"))
+os.chdir(wd)
+
+pdict = ReadParameterfile(wd, "Correction_Parameters.txt")
+
+out_dir = pdict["OUTPUT_LOC"]
+dsm_name = pdict["DSM_INPUT"]
+thermo_loc = pdict["THERMO_FOLDER"]
+time_info = pdict["TIME_INFO"]
+fv_name = pdict["FV_INPUT"]
+red_name = pdict["RED_INPUT"]
+
+cam_location = (float(pdict["CAMLOCATION_X"]),float(pdict["CAMLOCATION_Y"]),float(pdict["CAMLOCATION_Z"]))
+
+vertical_gradient = pdict["VERTICAL_GRAD"]
+em_const = pdict["EM_CONST"]
+if em_const == "NA":
+    em_const = 0.005
+else:
+    em_const = float(em_const)
+
+#soil_threshold = (float(pdict["SOIL_THRESHOLD"]),float(pdict["VEG_THRESHOLD"]))
+soil_lse = (float(pdict["LSE_VEGETATION"]),float(pdict["LSE_SOIL"]))
+
+### assigning values vor atmospheric radiance (pathradiance) and downwelling radiance (atmosphärische gegenstrahlung)
+L_atm = pdict["RADIANCE_AIR"]
+if L_atm == "NA":
+    L_atm = 0.001
+else:
+    L_atm = float(L_atm)
+L_down = pdict["RADIANCE_DOWN"]
+if L_down == "NA":
+    L_down = 324
+else:
+    L_down = float(L_down)
+
+
+# create output directory
+out_dir = OutDirectory(out_dir)
+
+##########################################################################################################################
+### READ INPUT FILES
+##########################################################################################################################
+
+### create filelist of input data and meteo data
+files_thermo = FileList(thermo_loc)
+
+# read fractional cover file and store as array
+with rasterio.open(fv_name, "r") as img:
+    fv_cover = img.read(1)
+    profile_fv = img.profile
+
+# read DSM file and store as array
+with rasterio.open(dsm_name, "r") as img:
+    dsm = img.read(1)
+    profile_dsm = img.profile
+    
+# read meteo data at camera 
+#meteo = pandas.read_csv(station_data,sep=",")
+# read meteo vertical profile
+gradient = pandas.read_csv(vertical_gradient,sep=",")
+# read time info for thermo raster
+timeinfo = pandas.read_csv(time_info,sep=",")
+
+
+
+### indexing dates in gradient data
+gradient_timeindex = []
+for i in range(0,len(gradient["time"])):
+    time_val = 24*60*int(gradient["time"][i][:2]) + 60*int(gradient["time"][i][11:13]) + int(gradient["time"][i][14:16])
+    gradient_timeindex.append(time_val)
+gradient["timeindex"] = gradient_timeindex
+timeinfo = timeinfo.set_index(timeinfo["Name"])
+
+### indexing dates in tmeinfo thermo data
+timeinfo_index = []
+for i in range(0,len(timeinfo["Datetime"])):
+    time_val = 24*60*int(timeinfo["Datetime"][i][:2]) + 60*int(timeinfo["Datetime"][i][11:13]) + int(timeinfo["Datetime"][i][14:16])
+    timeinfo_index.append(time_val) 
+timeinfo["timeindex"] = timeinfo_index
+timeinfo = timeinfo.set_index(timeinfo["Name"])
+#############################################################################################
+### EMISSIVITY
+#############################################################################################
+print("Start calculating EMISSIVITY...")
+
+# Calculating fractional vegetation cover
+
+### Calculate emissivity from Fv cover (Caselles 1996, kodimalar 2020)
+print("   Calculating LSE")
+
+
+# LSE for vegeatation and soil
+lse_veg = LseFromFv(fv_cover, soil_lse[0], soil_lse[1], em_const)
+
+# LSE for snow
+if red_name != "NA":
+    with rasterio.open(red_name,"r") as img:
+        red = img.read(1)
+    lse_veg = np.where(red >= 250, 0.98, lse_veg)
+else:
+    print("   no red channel available.")
+
+# LSE for rock
+#lse_rock = np.where(landclass == rock, 0.93, lse_snow)
+
+# LSE whole#
+# lse = lse_rock.copy
+lse = lse_veg.copy()
+
+
+
+##############################################################################################
+### Atmospheric correction
+#############################################################################################
+
+## Path length
+print("Start calculating PATH LENGTH")
+            
+# Creat x,y,z arrays for path length calculation   
+transform = profile_dsm["transform"]
+         
+y_ar = np.zeros(np.shape(dsm))          
+for i in range(np.shape(dsm)[0]):
+    y_ar[i,:] = i*transform[4] + transform[5]
+
+x_ar = np.zeros(np.shape(dsm))
+for j in range(np.shape(dsm)[1]):
+    x_ar[:,j] = j*transform[0] + transform[2]    
+
+# Calculate ground distance and height difference between XYZ and cam location
+ground_dist = np.where(dsm != -99999, np.sqrt((cam_location[0]-x_ar)**2 + (cam_location[1]-y_ar)**2), -99999)      
+height_diff = np.where(dsm != -99999, np.sqrt((dsm - cam_location[2])**2), -99999)        
+
+# Calculate path distance between XYZ and cam location
+path_ar = np.where(dsm != -99999, np.sqrt(ground_dist**2 + height_diff**2), -99999)        
+
+plt.imshow(path_ar)
+
+
+###################################################################################################
+### Atmospheric Correction
+print("Starting Atmospheric Correction")
+
+# iterating over all thermo images
+for nr in range(0,len(files_thermo)):
+    print ("   Preparing " + files_thermo[nr])
+    
+    ### read time info and extract time of thermo img
+    for a in range(0, len(timeinfo["timeindex"])):
+        if timeinfo["Name"][a] == files_thermo[nr][:-11]:
+            thermo_timeindex = timeinfo["timeindex"][a]
+   
+    
+    ### get correct coefficient and intersection for given timestep for T and RH
+    # get index of forwarding value
+    idx2 = pandas.Index(gradient["timeindex"])
+    gradientindex = idx2.get_loc(thermo_timeindex, method="ffill")
+    # get 
+    T_gradient = gradient["TL_coefficient"][gradientindex] + (thermo_timeindex - gradient["timeindex"][gradientindex]) * ((gradient["TL_coefficient"][gradientindex+1]-gradient["TL_coefficient"][gradientindex]) /(gradient["timeindex"][gradientindex+1]-gradient["timeindex"][gradientindex]))
+    T_intercept = gradient["TL_intercept"][gradientindex] + (thermo_timeindex - gradient["timeindex"][gradientindex]) * ((gradient["TL_intercept"][gradientindex+1]-gradient["TL_intercept"][gradientindex]) /(gradient["timeindex"][gradientindex+1]-gradient["timeindex"][gradientindex]))
+
+    RH_gradient = gradient["RH_coefficient"][gradientindex] + (thermo_timeindex - gradient["timeindex"][gradientindex]) * ((gradient["RH_coefficient"][gradientindex+1]-gradient["RH_coefficient"][gradientindex]) /(gradient["timeindex"][gradientindex+1]-gradient["timeindex"][gradientindex]))
+    RH_intercept = gradient["RH_intercept"][gradientindex] + (thermo_timeindex - gradient["timeindex"][gradientindex]) * ((gradient["RH_intercept"][gradientindex+1]-gradient["RH_intercept"][gradientindex]) /(gradient["timeindex"][gradientindex+1]-gradient["timeindex"][gradientindex]))
+    
+    T_cam = T_gradient * cam_location[2] + T_intercept
+    RH_cam = RH_gradient * cam_location[2] + RH_intercept
+            
+    # correct if not Kelvin
+    if T_cam < 200:
+        T_cam = T_cam + 271.15
+    
+    ### at cam values for RH
+    # correct if not in 0. form
+    if RH_cam > 2:
+        RH_cam = RH_cam / 100
+    H_air = RH_cam * MaxHumidity(T_cam)
+    
+    #######################################################################################################################
+    ## Mean path temperature and relative humidity
+    print("      Calculating Path variables")
+
+    
+    ## Calculating mean Pathtemperature
+    T_pix = np.where(dsm != -99999, (T_gradient * dsm + T_intercept) + 271.15, -99999)  
+    pathtemp = np.where(dsm != -99999, (T_pix + T_cam) / 2, -99999)
+    
+    ## Calculating mean Relative Humidity along path
+    #from measured T at cam location
+    #RH_pix = np.where(dsm != -99999, H_air / ((611.2*np.exp((22.46*T_pix)/(272.62+T_pix)))/(461.51*T_pix)), -99999)
+    # from gradient calculated from stations
+    RH_pix = np.where(dsm != -99999, RH_gradient * dsm + RH_intercept, -99999)
+    pathRH = np.where(dsm != -99999, (RH_pix + RH_cam) / 2, -99999)
+        
+    #########################################################################################################################################
+    ###Calculating Atmospheric transmission from Wiecek (2011)
+    
+    ### Calculating constant alpha from passman larmore tables (experimentally) wavelength 7.5-13ym
+    # as suggested by Minkina 2016    
+    T_ref = 288.15
+    RH_ref = 0.5 ### or 0.5??
+    d_ref = 1000
+    p_atm_ref = 0.8160
+    
+    ps_alpha = SatVapourPressure(T_ref)
+    alpha = -((Boltz() * T_ref)/((RH_ref*100) * ps_alpha * d_ref))*m.log(p_atm_ref)
+
+    
+    ### Calculate atmospheric transmission coefficient (tao)
+    print("      calculate Tao")
+    
+    ### calculate saturation vapour pressure
+    p_sat = np.where(pathtemp >= 273.15, 6.112 * np.exp(17.269*((pathtemp-273.17)/(pathtemp-35.86))), 6.112 * np.exp(21.874*((pathtemp-273.17)/(pathtemp-7.66))))
+    p_sat = np.where(pathtemp == -99999, -99999, p_sat)
+    
+    ### Calculate transmission coefficient
+    tao_atm = np.where(pathtemp != -99999, np.exp(-(alpha) * path_ar * ((p_sat*(pathRH*100))/(Boltz()*pathtemp))), -99999)
+    
+    
+    
+  
+    ##############################################################################################
+    ### Correction model
+    #############################################################################################
+    print("      Start correcting "+files_thermo[nr])
+    ### Corection based on Gillespie 1998 and Richter 2002
+    
+    with rasterio.open(thermo_loc+"/"+files_thermo[nr]) as img:
+        thermo1 = img.read(1)
+        profile_thermo = img.profile
+        
+    # downwelling radiance and path radiance are still missing!!!!
+    # reflected downwelling LWIR = (1-lse)*downradiance, atmosphärische gegenstrahlung ~324 W/m^-2
+    
+    # path radiance = LWIR atmosphärische eigenstrahlung corresponding to pathdistance
+    L_path = L_atm * path_ar 
+       
+    ### calculate at cam radiance from thermo   
+    # check if in Kelvin or degree
+    mean_th = np.mean(thermo1)
+    if mean_th < 100:
+        thermo = np.where(thermo1 != -99999, thermo1 + 271.15, -99999)
+    else:
+        thermo = thermo1
+        
+    # at cam radiance
+    L_cam = np.where(thermo != -99999, Boltz() * (thermo)**4, -99999)
+       
+    ### Correcting Surface radiance for each pixel
+    L_surf = np.where(thermo > 0, (L_cam - L_path - tao_atm*(1 - lse) * (L_down/m.pi)) / (tao_atm * lse), np.nan)
+    
+    ### Surface temperature in different units
+    if mean_th < 100:
+        T_surf = (L_surf / Boltz())**(1/4) - 271.15
+    else:
+        T_surf = (L_surf / Boltz())**(1/4)
+    
+    # save corrected surface temperature
+    print("      Saving...")  
+    profile_thermo["dtype"] = T_surf.dtype
+    with rasterio.open(f"OUTPUT/{files_thermo[nr]}", "w", **profile_thermo) as dst:
+        dst.write(T_surf,1)
+
+
+
+print("   done.")
+
+
+print("Save additional info")
+
+# save lse
+profile_dsm["dtype"] = lse.dtype
+with rasterio.open("OUTPUT/LSE.tif", "w", **profile_dsm) as dst:
+    dst.write(lse,1)
+    
+# save tao
+profile_thermo["dtype"] = tao_atm.dtype
+with rasterio.open("OUTPUT/Tao.tif", "w", **profile_thermo) as dst:
+    dst.write(tao_atm,1)
+
+# profile_thermo["dtype"] = path_ar.dtype
+# with rasterio.open("OUTPUT/path_ar.tif", "w", **profile_thermo) as dst:
+#     dst.write(path_ar,1)
+
+# profile_thermo["dtype"] = pathRH.dtype
+# with rasterio.open("OUTPUT/pathRH.tif", "w", **profile_thermo) as dst:
+#     dst.write(pathRH,1)
+
+# profile_thermo["dtype"] = L_cam.dtype
+# with rasterio.open("OUTPUT/thermo.tif", "w", **profile_thermo) as dst:
+#     dst.write(L_cam,1)
+
+
+
+
+
